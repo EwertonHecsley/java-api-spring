592ebb335cec93b4db26f9d8a2b324b9c3bc00dc
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-03-26 19:39:36 -0300

    adicionando Request Mapping

--- src/main/java/person/apirest/controller/UserController.java
+++ src/main/java/person/apirest/controller/UserController.java
@@ -5,23 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import person.apirest.model.User;
 import person.apirest.repository.UserRepository;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/user")
+    @GetMapping()
     public List<User> listAll(){
         return userRepository.findAllUsers();
     }
 
-    @GetMapping("/user/{login}")
+    @GetMapping("/{login}")
     public User getUser(@PathVariable("login") String login){
         return userRepository.findUserLogin(login);
     }
